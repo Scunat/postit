7728b55be9deb3c75bdc3fd5d4a13c356aed0923
Sarita Cunat <cunat.sarita@gmail.com>
2020-03-20 16:46:39 +0100

    Bouger le postit

--- js/CPostit.js
+++ js/CPostit.js
@@ -24,10 +24,22 @@ class postit {
   changeColor(color) {
     this.color = color;
   }
-
   affichePostit() {
-    let myElement = document.createElement("div");
+    let myElement;
+    let creation = false;
+
+    if (document.getElementById('bouge') == null) {
+       
+        myElement = document.createElement('div');
+        creation = true;
+    }
+    else {
+        console.log("Mon elem Existe")
+        myElement = document.getElementById('bouge');
+    }
+  
     myElement.style.position = "fixed";
+    myElement.id = "bouge"
     myElement.style.top = this.y + "px";
     myElement.style.left = this.x + "px";
     myElement.style.width = "150px";
@@ -35,9 +47,47 @@ class postit {
     myElement.style.background = "yellow";
     myElement.style.padding = "5px";
     myElement.style.color = "black";
-    myElement.innerHTML = "postit" + ' ' + this.speed;
+    myElement.innerHTML = "postit" + " " + this.speed;
+    myElement.style.backgroundRepeat = "no-repeat";
+    myElement.style.backgroundSize = "contain";
     document.body.appendChild(myElement);
+
+    myElement.addEventListener("click", () => {
+      console.log("on bouge !");
+      move = true;
+    });
+    if (postit) {
+      document.body.appendChild(myElement);
+    }
   }
 }
 myPostit = new postit(100, 100, 30, "blue");
 myPostit.affichePostit();
+
+//Faire bouger le postit avec des évènements
+let move = false;
+let x;
+let y;
+
+let myPostits = new postit("black", 50, 50, 12);
+myPostit.affichePostit();
+
+document.addEventListener("mousemove", e => {
+  x = e.clientX;
+  y = e.clientY;
+});
+document.addEventListener("mouseup", () => {
+  console.log("on stop !");
+  move = false;
+});
+
+function refresh() {
+  if (move) {
+    console.log("on déplace !");
+    myPostits.changePlace(x, y);
+    myPostits.affichePostit();
+  }
+  setTimeout(refresh, 300);
+}
+
+refresh();
